031eb25b8c937ce8e4641f57e4c1ef943a98465b
vyking69 <139842940+vyking69@users.noreply.github.com>
2023-09-22 09:48:59 -0700

    simple get request test update

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.fixture
+def _root_url():
+    return "http://127.0.0.1:5000"
+
--- tests/test_unit_cases.py
+++ tests/test_unit_cases.py
@@ -1,9 +1,22 @@
 import pytest
+from utils.managers import send_get_request
 
-def test_form_polygon_request():
+def test_get_request(_root_url):
+    response = send_get_request(f"{_root_url}/get-data")
+
+    # Check the response status code
+    assert response.status_code == 200, "response not 200 -- issue with request"
+        # If the response is successful (status code 200), parse the JSON data
+    data = response.json()
+
+    print("Data received:")
+    print(data)
+
+
+def test_form_polygon_request(_root_url): 
     return 1
 
-def test_send_polygon_request():
+def test_send_polygon_request(_root_url):
     return 1
 
 def test_requesting_random_words():
